65714e20ba1314f93fc752a6bb11485072e2a9ad
rws <1005801372@qq.com>
2025-02-21 15:00:54 +0800

    注释1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="wrapper">
     <div class="loginTitle">
-      <img src="@/assets/image/Home/login1.png"
-           style="position: relative; width:700px; height: 75px; top: 67px;"
-           alt=""/>
+      <img src="@/assets/image/Home/login1.png" style="position: relative; width:700px; height: 75px; top: 67px;" alt="" />
       <h1 style="color: #75fee1;position: relative;text-align: center;top: -15px; letter-spacing: 18px;">
-        交通大数据可视化协同平台</h1>
+        企业管理平台</h1>
 
     </div>
     <div class="login-div">
@@ -17,8 +15,7 @@
           <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-user" v-model="user.username"></el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password
-                    v-model="user.password"></el-input>
+          <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password v-model="user.password"></el-input>
         </el-form-item>
         <el-form-item style="margin: 10px 0; text-align: center">
           <el-button type="primary" size="small" autocomplete="off" @click="login">登录</el-button>
@@ -35,32 +32,32 @@ import navigationStore from '@/store/NavigationStore'
 
 export default {
   name: 'Login',
-  data() {
+  data () {
     return {
       user: {},
       rules: {
         username: [
-          {required: true, message: '请输入用户名', trigger: 'blur'}
+          { required: true, message: '请输入用户名', trigger: 'blur' }
         ],
         password: [
-          {required: true, message: '请输入密码', trigger: 'blur'}
+          { required: true, message: '请输入密码', trigger: 'blur' }
         ]
       }
     }
   },
 
-  beforeCreate() {
+  beforeCreate () {
     if (localStorage.getItem('user')) {
       this.$router.push('/home')
     }
   },
 
-  mounted() {
+  mounted () {
     // 登录绑定事件,使用enter登录
     window.addEventListener('keydown', this.keyDown)
   },
   methods: {
-    login() {
+    login () {
       this.$refs['userForm'].validate((valid) => {
         if (valid) {
           this.request.post('/user/login', this.user).then(res => {
@@ -81,7 +78,7 @@ export default {
         }
       })
     },
-    keyDown(e) {
+    keyDown (e) {
       //如果是回车则执行登录方法
       if (e.keyCode == 13) {
         this.login()
@@ -94,7 +91,7 @@ export default {
 <style>
 .wrapper {
   height: 100vh;
-  background-image: url("@/assets/image/Home/login.jpg");
+  background-image: url('@/assets/image/Home/login.jpg');
   background-size: 100% 100%;
   background-repeat: no-repeat;
   overflow: hidden;
@@ -108,7 +105,11 @@ export default {
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.2); /* 背景颜色为白色，透明度为50% */
   color: #75fee1; /* 文字颜色，确保在深色背景上可见 */
-  border-image: linear-gradient(to right, #FFFFFF, #10ac84); /* 设置边框渐变色 */
+  border-image: linear-gradient(
+    to right,
+    #ffffff,
+    #10ac84
+  ); /* 设置边框渐变色 */
   border-image-slice: 1; /* 确保渐变覆盖整个边框 */
   box-shadow: 0 0 3px rgba(0, 123, 255, 0.5); /* 添加蓝色阴影增加层次感 */
 }
